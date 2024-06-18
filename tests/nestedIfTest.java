33a1a425deec20868b1d3f14d598642cb4ca0ee9
devinburger <devinjburger@gmail.com>
2024-06-18 12:34:14 -0400

    June 18th

---
+++ tests/nestedIfTest.java
@@ -0,0 +1,13 @@
+public class MyClass {
+    public int something(int a, int b) {
+         int x = a + b;    
+         if (x > 0) { 
+            if (y < x){
+                x = x * -1;   
+                System.out.println(“hello”);
+            }     
+            x + y; 
+         }
+         return x;
+    }
+}
