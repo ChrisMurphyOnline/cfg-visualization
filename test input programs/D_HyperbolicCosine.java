58b9430f5850f8d974750e48e67064aca60b6f3a
Chris Murphy <cdmurphy@seas.upenn.edu>
2024-07-15 10:03:41 -0400

    added line numbers

--- test input programs/D_HyperbolicCosine.java
+++ test input programs/D_HyperbolicCosine.java
@@ -1,52 +1,52 @@
 public class D_HyperbolicCosine {
 	public static double cosh(double x) {
-		if (x != x) {
-			return x;
+		print("1"); if (x != x) {
+			print("2"); return x;
 		}
-		if (x > 20) {
-			if (x >= LOG_MAX_VALUE) {
-				final double t = Math.exp(0.5 * x);
-				return (0.5 * t) * t;
+		print("4"); if (x > 20) {
+			print("5"); if (x >= LOG_MAX_VALUE) {
+				print("6"); final double t = Math.exp(0.5 * x);
+				print("7"); return (0.5 * t) * t;
 			} 
 			else {
-				return 0.5 * Math.exp(x);
+				print("10"); return 0.5 * Math.exp(x);
 			}
 		} 
 		else {
-			if (x < -20) {
-				if (x <= -LOG_MAX_VALUE) {
-					final double t = Math.exp(-0.5 * x);
-					return (0.5 * t) * t;
+			print("14"); if (x < -20) {
+				print("15"); if (x <= -LOG_MAX_VALUE) {
+					print("16"); final double t = Math.exp(-0.5 * x);
+					print("17"); return (0.5 * t) * t;
 				} 
 				else {
-					return 0.5 * Math.exp(-x);
+					print("20"); return 0.5 * Math.exp(-x);
 				}
 			}
 		}
-		final double hiPrec[] = new double[2];
-		if (x < 0.0) {
-			x = -x;
+		print("24"); final double hiPrec[] = new double[2];
+		print("25"); if (x < 0.0) {
+			print("26"); x = -x;
 		}
-		FastMath.exp(x, 0.0, hiPrec);
-		double ya = hiPrec[0] + hiPrec[1];
-		double yb = -(ya - hiPrec[0] - hiPrec[1]);
-		double temp = ya * HEX_40000000;
-		double yaa = ya + temp - temp;
-		double yab = ya - yaa;
-		double recip = 1.0/ya;
-		temp = recip * HEX_40000000;
-		double recipa = recip + temp - temp;
-		double recipb = recip - recipa;
-		recipb += (1.0 - yaa*recipa - yaa*recipb - yab*recipa - yab*recipb) * recip;
-		recipb += -yb * recip * recip;
-		temp = ya + recipa;
-		yb += -(temp - ya - recipa);
-		ya = temp;
-		temp = ya + recipb;
-		yb += -(temp - ya - recipb);
-		ya = temp;
-		double result = ya + yb;
-		result *= 0.5;
-		return result;
+		print("28"); FastMath.exp(x, 0.0, hiPrec);
+		print("29"); double ya = hiPrec[0] + hiPrec[1];
+		print("30"); double yb = -(ya - hiPrec[0] - hiPrec[1]);
+		print("31"); double temp = ya * HEX_40000000;
+		print("32"); double yaa = ya + temp - temp;
+		print("33"); double yab = ya - yaa;
+		print("34"); double recip = 1.0/ya;
+		print("35"); temp = recip * HEX_40000000;
+		print("36"); double recipa = recip + temp - temp;
+		print("37"); double recipb = recip - recipa;
+		print("38"); recipb += (1.0 - yaa*recipa - yaa*recipb - yab*recipa - yab*recipb) * recip;
+		print("39"); recipb += -yb * recip * recip;
+		print("40"); temp = ya + recipa;
+		print("41"); yb += -(temp - ya - recipa);
+		print("42"); ya = temp;
+		print("43"); temp = ya + recipb;
+		print("44"); yb += -(temp - ya - recipb);
+		print("45"); ya = temp;
+		print("46"); double result = ya + yb;
+		print("47"); result *= 0.5;
+		print("48"); return result;
 	}
 }
